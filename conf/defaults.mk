41bb644a4d0fd26ee29c0eb3e9f6b4aafc02a801
Laurent Bercot <ska-skaware@skarnet.org>
2023-02-06 13:36:41 +0000

    Add powerpc64 and powerpc64le archs; prepare for 3.1.4.0
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- conf/defaults.mk
+++ conf/defaults.mk
@@ -1,14 +1,14 @@
 # This file normally shouldn't be changed; the values can be
 # overridden by invoking make with arguments.
-# e.g.: make SHEBANGDIR=/usr/bin VERSION=3.1.1.0
+# e.g.: make SHEBANGDIR=/usr/bin VERSION=3.1.4.0
 
 # The version of the software being built.
-VERSION := 3.1.3.0
+VERSION := 3.1.4.0
 
 # Where stuff is going to be built. Change for out-of-tree builds.
 OUTPUT := output
 
-# Where to find the execlineb binary.
+# Where to find the execlineb program.
 # Change if building for a distro that provides its own skaware packages.
 SHEBANGDIR := /command
 
